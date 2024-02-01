a340d200d0d805946885a32e6c5ac04f36f40ebe
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-02-01 19:29:02 +0530

    Added sample.

---
+++ NativeEmbedding_DataForm/NativeEmbedding_DataForm/ViewModel/DataFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NativeEmbedding_DataForm
+{
+    public class DataFormViewModel
+    {
+        public DataFormModel DataFormModel { get; set; }
+        public DataFormViewModel()
+        {
+            this.DataFormModel = new DataFormModel();
+        }
+    }
+}
